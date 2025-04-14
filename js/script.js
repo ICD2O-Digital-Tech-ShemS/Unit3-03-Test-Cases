cd45619e0594ee2aa13c51bf1523e7eb0e8a910f
Shem Irekpita <Shem.irekpita@stu.ocsb.ca>
2025-04-14 14:50:28 +0000

    This is the calculation of the volume of a sphear

--- js/script.js
+++ js/script.js
@@ -1,5 +1,16 @@
 // Copyright (c) 2020 Mr. Coxall All rights reserved
 //
-// Created by: Mr. Coxall
-// Created on: Sep 2020
+// Created by: Shem Irekpita
+// Created on:Aipril 2025
 // This file contains the JS functions for index.html
+"use strict";
+    function calculate() {
+        // Get the input values
+        let radius = parseFloat(document.getElementById("radius").value);
+
+        // Calculate
+        let Volume =(4/3) * radius**3 * Math.PI;
+
+        // Display the results
+        document.getElementById("Volume").innerHTML = "Volume " + Volume.toFixed(2) + "cm";
+}
